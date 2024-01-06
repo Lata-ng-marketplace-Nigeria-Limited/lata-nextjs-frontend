f2eca0c56b652e70b6794d1a43500c9873949007
ansman58 <javanslem@gmail.com>
2024-01-06 17:48:20 +0100

    fix update product

--- src/analytics/GoogleAnalytics.tsx
+++ src/analytics/GoogleAnalytics.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-
 import { IEnv } from "@/interface/general";
 import Script from "next/script";
 import React from "react";
--- src/components/product/ViewProductArea.tsx
+++ src/components/product/ViewProductArea.tsx
@@ -19,10 +19,6 @@ export const ViewProductArea = ({ data }: Props) => {
   const [productNotFound, setProductNotFound] = useState(false);
 
   useEffect(() => {
-    console.log({ data });
-  }, [product]);
-
-  useEffect(() => {
     if (!data) {
       setProductNotFound(true);
     } else {
