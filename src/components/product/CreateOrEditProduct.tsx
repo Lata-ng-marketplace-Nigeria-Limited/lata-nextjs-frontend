4c80f565e648e4786c8054d0c45deb182837c86e
ansman58 <javanslem@gmail.com>
2024-01-23 01:33:27 +0100

    fix discount type error

--- src/components/product/CreateOrEditProduct.tsx
+++ src/components/product/CreateOrEditProduct.tsx
@@ -21,7 +21,7 @@ export const CreateOrEditProduct = ({ data }: Props) => {
     state: "",
     city: "",
     categoryId: "",
-    discount: "",
+    discount: 0,
     subCategoryId: "",
   });
   const [selectedPhotos, setSelectedPhotos] = useState<SelectedImagePreview>();
