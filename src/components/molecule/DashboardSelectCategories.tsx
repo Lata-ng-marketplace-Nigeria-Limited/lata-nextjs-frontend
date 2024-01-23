41c77a588fd220b44d8c6ded53d3002701542bfc
ansman58 <javanslem@gmail.com>
2024-01-23 18:01:31 +0100

    bring back buy here button

--- src/components/molecule/DashboardSelectCategories.tsx
+++ src/components/molecule/DashboardSelectCategories.tsx
@@ -88,7 +88,7 @@ export const DashboardSelectCategories = () => {
 
       <Button
         format="primary"
-        className="min-w-[150px] sm:min-w-[174px] opacity-0 pointer-events-none"
+        className="min-w-[150px] sm:min-w-[174px]"
         onClick={handleShowModal}
       >
         BUY HERE
