189090a902e3bf03bf838849cf78420d7bf09eca
ansman58 <javanslem@gmail.com>
2024-03-24 15:38:54 +0100

    remove unused location types

--- src/interface/location.ts
+++ src/interface/location.ts
@@ -1,28 +1,3 @@
-export interface ICountriesData {
-  name: string;
-  dial_code: string;
-  code: string;
-  flag: string;
-}
-
-export interface IStatesData {
-  label: string;
-  value: string;
-  cities: ICitiesData[];
-}
-export interface ICitiesData {
-  name: string;
-  id: number;
-}
-
-export interface ICountriesData {
-  name: string;
-  dial_code: string;
-  code: string;
-  flag: string;
-}
-
-
 export interface State {
   id: string;
   name: string;
