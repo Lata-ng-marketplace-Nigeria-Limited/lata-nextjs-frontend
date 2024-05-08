fb814e47338682f366e94d2859e4ebc9ccee24aa
ansman58 <javanslem@gmail.com>
2024-05-08 19:34:33 +0100

    update ads bug

--- src/app/external-services/GoogleAdsUnit.tsx
+++ src/app/external-services/GoogleAdsUnit.tsx
@@ -9,12 +9,48 @@ const GoogleAdsUnit = ({ ...props }: Props) => {
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
+  // React.useEffect(() => {
+  //   const loadAds = () => {
+  //     let adsbygoogle = (window as any).adsbygoogle;
+  //     try {
+  //       if (adsbygoogle && !adsbygoogle?.loaded) return;
+      
+  //       const adsElements = document.getElementsByClassName("adsbygoogle");
+  //       console.log("adsElements", adsElements);
+  //       if (adsbygoogle) {
+  //         (adsbygoogle = adsbygoogle || []).push({});
+
+  //         console.log("ads length", adsbygoogle?.length);
+
+  //         // window.addEventListener("load", function () {
+  //         //   ((window as any).adsbygoogle =
+  //         //     (window as any).adsbygoogle || []).push({});
+  //         // });
+
+  //         console.log("Ads with content", (window as any).adsbygoogle);
+  //       }
+  //     } catch (err) {
+  //       console.log(err);
+  //     }
+  //   };
+  //   // console.log("Ads", Array.isArray((window as any).adsbygoogle) );
+  //   // console.log("Ads", (window as any).adsbygoogle) ;
+  //   loadAds();
+  // }, [pathname, searchParams]);
+
   React.useEffect(() => {
     const loadAds = () => {
       try {
-        if ((window as any).adsbygoogle) {
-          ((window as any).adsbygoogle =
-            (window as any).adsbygoogle || []).push({});
+        const adsElements = document.getElementsByClassName("adsbygoogle");
+        for (let i = 0; i < adsElements.length; i++) {
+          const adsElement = adsElements[i] as HTMLElement;
+          if (adsElement.childNodes.length === 0) {
+            // If the 'ins' element is empty, push the ad
+            if ((window as any).adsbygoogle) {
+              ((window as any).adsbygoogle =
+                (window as any).adsbygoogle || []).push({});
+            }
+          }
         }
       } catch (err) {
         console.log(err);
@@ -22,13 +58,14 @@ const GoogleAdsUnit = ({ ...props }: Props) => {
     };
     loadAds();
   }, [pathname, searchParams]);
+  
 
   return (
     <ins
-      className="adsbygoogle adbanner-customize"
-      style={{ display: "block", width: "100%" }}
+      className="adsbygoogle"
+      style={{ display: "block" }}
       data-ad-client={process.env.NEXT_PUBLIC_GOOGLE_ADS_CLIENT_ID}
-      data-ad-slot="2711689551"
+      data-ad-slot="4039248860"
       data-ad-format="auto"
       data-full-width-responsive="true"
       {...props}
