b53edbabeef1cb335b8323824597d571429b7e33
ansman58 <javanslem@gmail.com>
2026-04-06 14:14:52 +0100

    update backend urls

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,4 +1,4 @@
-export const API_URL_2 = process.env.NEXT_PUBLIC_LATA_API_URL_2 || "https://lata-backend-production.up.railway.app/v1";
-export const API_URL = process.env.NEXT_PUBLIC_LATA_API_URL || "https://lata-backend-production.up.railway.app/v1";
-export const SOCKET_API_URL = process.env.NEXT_PUBLIC_CHAT_API_URL || "wss://lata-chat-production.up.railway.app/";
+export const API_URL_2 = process.env.NEXT_PUBLIC_LATA_API_URL_2 || "https://api.lata.ng/v1";
+export const API_URL = process.env.NEXT_PUBLIC_LATA_API_URL || "https://api.lata.ng/v1";
+export const SOCKET_API_URL = process.env.NEXT_PUBLIC_CHAT_API_URL || "wss://chat.lata.ng/";
 
