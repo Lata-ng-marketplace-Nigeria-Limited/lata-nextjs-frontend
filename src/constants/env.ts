3a726bb111336f6c522736720b71a6c57b570ce3
ansman58 <javanslem@gmail.com>
2026-04-06 15:29:08 +0100

    update chat url

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,5 +1,5 @@
 export const API_URL_2 = process.env.NEXT_PUBLIC_LATA_API_URL_2 || "https://api.lata.ng/v1";
 export const API_URL = process.env.NEXT_PUBLIC_LATA_API_URL || "https://api.lata.ng/v1";
-// export const SOCKET_API_URL = process.env.NEXT_PUBLIC_CHAT_API_URL || "wss://chat.lata.ng/";
-export const SOCKET_API_URL = process.env.NEXT_PUBLIC_CHAT_API_URL || "wss://localhost:3404";
+export const SOCKET_API_URL = process.env.NEXT_PUBLIC_CHAT_API_URL || "wss://chat.lata.ng/";
+// export const SOCKET_API_URL = process.env.NEXT_PUBLIC_CHAT_API_URL || "wss://localhost:3404";
 
