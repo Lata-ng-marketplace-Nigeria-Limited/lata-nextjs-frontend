844e95521864a4eb6c6607e4e9bb96535d9e424c
ansman58 <javanslem@gmail.com>
2026-09-26 15:31:39 +0100

    fix(ui): pass correct state name to ProductCard location prop

--- src/components/product/LazyLoadProducts.tsx
+++ src/components/product/LazyLoadProducts.tsx
@@ -8,7 +8,7 @@ import ProductGridList from "@atom/ProductGridList";
 import React, { useEffect, useState } from "react";
 import { useIntersectionObserver } from "usehooks-ts";
 import { ProductListSkeleton } from "@components/skeleton/ProductCardSkeleton";
-import { selectedCity } from "@/utils/location";
+import { selectedCity, selectedState } from "@/utils/location";
 import { State } from "@/interface/location";
 import RollerSpinner from "@/components/molecule/RollerSpinner";
 
@@ -90,10 +90,9 @@ export default function LazyLoadProducts(props: Props) {
               price={formatPrice(product?.price)}
               productName={product?.name}
               description={product?.description}
-              state={selectedCity(
+              state={selectedState(
                 props.statesInNigeria,
                 product?.state,
-                product?.city,
               )}
               city={selectedCity(
                 props.statesInNigeria,
